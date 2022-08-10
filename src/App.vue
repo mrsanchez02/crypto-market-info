8d0a3869ddf1a62b615665c24b4b0ef734de4b01
Mr Jose Sanchez <leandrosanchez.it@gmail.com>
2022-08-10 16:38:07 -0400

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div className="container">
+    <div className="row">
+      <h1 className="display-1 mt-4">Crypto Market</h1>
+      <h4 className="fw-light">{{ msg }}</h4>
+      <input 
+        type="text" 
+        placeholder="Search coin..." 
+        className='text-light form-control bg-dark rounded-0 border-0 my-4' 
+        @keyup="searchCoin"
+        v-model="textSearch"
+      />
+      <table className="table table-dark table-hover">
+        <caption className="caption-top">Powered by <a href="https://www.coingecko.com/" className="link-light text-decoration-none">CoinGecko</a></caption>
+        <thead>
+          <tr>
+            <th v-for="(title,index) in titles" :key="index">{{title}}</th>
+          </tr>
+        </thead>
+        <tbody className="table-group-divider">
+          <tr v-for="(coin, index) in filteredCoins" :key="coin.id">
+            <td className="text-muted">
+              {{index+1}}
+            </td>
+            <td>
+              <img :src="coin.image" style="width: 2rem" className="me-2" alt="a coin thumbnail">
+              <span>
+                {{coin.name}}
+              </span>
+              <span className="ms-2 text-uppercase text-muted">
+                {{coin.symbol}}
+              </span>
+            </td>
+            <td>
+              $ {{coin.current_price}}
+            </td>
+            <td :className="[coin.price_change_percentage_24h>0 ? 'text-success' : 'text-danger']">
+              {{coin.price_change_percentage_24h.toFixed(2)}} %
+            </td>
+            <td>
+              $ {{coin.total_volume.toLocaleString()}}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      msg: "Hello Crypto users!",
+      coins: [],
+      filteredCoins:[],
+      titles: ['#','Coin','Price','Price Change 24h','24h volume'],
+      textSearch: ""
+    };
+  },
+  async mounted() {
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    );
+    const data = await res.json();
+    this.coins = data;
+    this.filteredCoins = data;
+  },
+  methods:{
+    searchCoin(){
+      this.filteredCoins = this.coins.filter(coin => 
+        coin.name.toLowerCase().includes(this.textSearch.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(this.textSearch.toLowerCase())
+      );
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
